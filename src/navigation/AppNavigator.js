fb40b16ec496016b409aec3d7a35bf2096f817bb
ortizProg <santiagoortiz012@gmail.com>
2025-10-27 20:59:22 -0500

    Fixed conflixs in app nativator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,30 +1,46 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from '../screens/HomeScreen';
-import ChatsScreen from '../screens/ChatsScreen';
-import AddPublicationScreen from '../screens/AddPublicationScreen';
-import EventsScreen from '../screens/EventsScreen';
-import SettingsScreen from '../screens/SettingsScreen';
-import TabBar from '../components/TabBar';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import HomeScreen from "../screens/HomeScreen";
+import ChatsScreen from "../screens/ChatsScreen";
+import AddPublicationScreen from "../screens/AddPublicationScreen";
+import EventsScreen from "../screens/EventsScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import DiscussionDetailScreen from "../screens/DiscussionDetailScreen";
+import TabBar from "../components/TabBar";
+import LoginScreen from '../screens/LoginScreen';
+import SignUpScreen from '../screens/SignUpScreen';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 function Tabs() {
   return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBar={(props) => <TabBar {...props} />}
+      screenOptions={{ headerShown: false }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Chats" component={ChatsScreen} />
+      <Tab.Screen name="AddPublication" component={AddPublicationScreen} />
+      <Tab.Screen name="Events" component={EventsScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Login" component={LoginScreen} />
+      <Tab.Screen name="Register" component={SignUpScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function AppNavigator() {
+  return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        tabBar={(props) => <TabBar {...props} />}
-        screenOptions={{ headerShown: false }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Chats" component={ChatsScreen} />
-        <Tab.Screen name="AddPublication" component={AddPublicationScreen} />
-        <Tab.Screen name="Events" component={EventsScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="MainTabs" component={Tabs} />
+        <Stack.Screen name="DiscussionDetail" component={DiscussionDetailScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
